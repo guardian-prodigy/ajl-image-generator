becd2131dfa9a3146b4f1d2965894f3085c3ea6a
Guardian Prodigy <hahdhdhwj@gmail.com>
2023-05-14 22:23:10 +0200

    AJL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,8 @@ const App = () => {
     <Router>
       <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
         <Link to={"/"}>
-          <img src={logo} alt="logo" className="w-28 object-contain" />
+          {/* <img src={logo} alt="logo" className="w-28 object-contain" /> */}
+          AJL
         </Link>
         <Link
           to="/create-post"
